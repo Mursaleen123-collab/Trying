be5471d5a2242cfaf3cd3fecde982eace670aaec
Mursaleen123-collab <109733203+Mursaleen123-collab@users.noreply.github.com>
2022-08-17 00:53:37 -0700

    g

---
+++ student/student-quiz-sheet.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Portal - Bootstrap 5 Admin Dashboard Template For Developers</title>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- FontAwesome JS-->
+	<script defer src="assets/plugins/fontawesome/js/all.min.js"></script>
+
+	<!-- App CSS -->
+	<link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
+
+</head>
+
+<body class="app">
+	<?php include('navbar.php'); ?>
+
+	<div class="app-wrapper">
+
+		<div class="app-content pt-3 p-md-3 p-lg-4">
+			<div class="container-xl">
+
+
+				<div class="col-12 col-lg-10 mx-auto " id="print-invoice">
+					<div class="app-card app-card-chart h-100 shadow-sm">
+						<!--//app-card-header-->
+						<div class="app-card-body p-3 p-lg-4">
+							<div class=" row">
+								<div class="col-xs-6 col-sm-6 col-md-6">
+									<div class="receipt-left">
+										<img class="img-responsive" alt="iamgurdeeposahan" src="assets\images\invoice-logo.jpg" style="width: 71px; border-radius: 43px;">
+									</div>
+								</div>
+								<div class="col-xs-6 col-sm-6 col-md-6 text-end">
+									<div class="receipt-right">
+										<h5>Royal College of Managment & Sciences</h5>
+										<p>055-3250765 <i class="fa fa-phone"></i></p>
+										<p>Royal@gmail.com <i class="fa fa-envelope-o"></i></p>
+										<p>Royal College Paracha Center <i class="fa fa-location-arrow"></i></p>
+									</div>
+								</div>
+							</div>
+
+							<div class=" row">
+								<div class=" col-md-8 text-left">
+									<div class="text-start receipt-right">
+										<h5>Mursaleen Umer </h5>
+										<p><b>Mobile :</b> 0335-4961946</p>
+										<p><b>Email :</b> bthunder418@gmail.com</p>
+										<p><b>Address :</b> Khaqan Street Arif Colony Gill Road Gujranwala</p>
+									</div>
+								</div>
+								<div class="col-md-4 ">
+									<div class="text-end pt-3">
+										<h3>INVOICE # 102</h3>
+									</div>
+								</div>
+							</div>
+
+							<div>
+								<table class="table table-bordered">
+									<thead>
+										<tr>
+											<th>Description</th>
+											<th>Amount</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td class="col-md-9">Payment for 6th installment</td>
+											<td class="col-md-3">15,000/-</td>
+										</tr>
+										<tr>
+											<td class="text-right">
+												<p>
+													<strong>Total Amount: </strong>
+												</p>
+												<p>
+													<strong>Late Fees: </strong>
+												</p>
+												<p>
+													<strong>Payable Amount: </strong>
+												</p>
+												<p>
+													<strong>Balance Due: </strong>
+												</p>
+											</td>
+											<td>
+												<p>
+													<strong> 65,500/-</strong>
+												</p>
+												<p>
+													<strong> 500/-</strong>
+												</p>
+												<p>
+													<strong> 1300/-</strong>
+												</p>
+												<p>
+													<strong> 9500/-</strong>
+												</p>
+											</td>
+										</tr>
+										<tr>
+
+											<td class="text-right">
+												<h2><strong>Total: </strong></h2>
+											</td>
+											<td class="text-left text-danger">
+												<h2><strong> 31.566/-</strong></h2>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+
+							<div class="row p-3">
+								<div class="col-xs-8 col-sm-8 col-md-8 text-left">
+									<div class="receipt-right">
+										<p><b>Date :</b> 15 Aug 2016</p>
+										<h5 style="color: rgb(140, 140, 140);">Thanks!</h5>
+									</div>
+								</div>
+								<div class="col-xs-4 col-sm-4 col-md-4">
+									<div class="text-end ">
+										<h5>Stamp</h5>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+					<!--//app-card-body-->
+				</div>
+				<!--//app-card-->
+
+			</div>
+			<!--//container-fluid-->
+		</div>
+		<!--//app-content-->
+
+	</div>
+	<!--//app-wrapper-->
+	<!-- Javascript -->
+	<script src="assets/plugins/popper.min.js"></script>
+	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Charts JS -->
+	<script src="assets/plugins/chart.js/chart.min.js"></script>
+	<script src="assets/js/index-charts.js"></script>
+
+	<!-- Page Specific JS -->
+	<script src="assets/js/app.js"></script>
+
+</body>
+
+</html>
