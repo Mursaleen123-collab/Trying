cc6114c846b13f3eb1b26a07508584e0f78541de
Mursaleen123-collab <109733203+Mursaleen123-collab@users.noreply.github.com>
2022-08-18 09:12:48 -0700

    g

--- faq.php
+++ faq.php
@@ -45,10 +45,10 @@
                 <div class="col-md-6 p-4">
                     <img src="assets/img/faq.svg" alt="" class="img-thumbnail ">
                 </div>
-                <div class="col-md-6 p-4">
+                <div class="col-md-6 p-4 ">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
+                            <h2 class="accordion-header " id="headingOne">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                     How new visitor can apply for Student / Teacher ?
                                 </button>
@@ -56,12 +56,12 @@
                             <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                 <div class="accordion-body">
                                     <strong>Download the Given PDF and submit To Admin Office</strong>
-                                    <div class="row">
+                                    <div class="row mt-4">
                                         <div class="col-md-6 text-end">
-                                            <a href="#" class="btn btn-info  text-white" download>Teacher</a>
+                                            <a href="#" class="btn btn-outline-success" download>Teacher</a>
                                         </div>
                                         <div class="col-md-6">
-                                            <a href="#" class="btn btn-info  text-white" download>Student</a>
+                                            <a href="#" class="btn btn-outline-success" download>Student</a>
                                         </div>
                                     </div>
                                 </div>
@@ -80,14 +80,28 @@
                             </div>
                         </div>
                         <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <h2 class="accordion-header" id="headingThree">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                     How Student can be login
                                 </button>
                             </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                                 <div class="accordion-body">
                                     Staff be login through the provided link on the search bar <code>https://lms-mcr.herokuapp.com/student/student-login.php</code>,Or you can vist the footer.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingFour">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Password at your first login
+                                </button>
+                            </h2>
+                            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    your CNIC and you DOB year like 2000 , 1999 is your first password.
+                                    <code>34101970577031999</co>
                                 </div>
                             </div>
                         </div>
